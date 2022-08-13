e273097b76054a5687f14c6a49d5caa8e3af5705
Lincoln <lincoln@itmedicus.com>
2022-08-13 16:49:27 +0600

    init

--- app/src/main/java/com/example/mybaseproject2/ui/fragments/notifications/Notifications.kt
+++ app/src/main/java/com/example/mybaseproject2/ui/fragments/notifications/Notifications.kt
@@ -1,15 +1,7 @@
 package com.example.mybaseproject2.ui.fragments.notifications
 
-import androidx.lifecycle.ViewModelProvider
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.viewModels
-import com.example.mybaseproject2.R
 import com.example.mybaseproject2.base.BaseFragment
-import com.example.mybaseproject2.databinding.FragmentGalleryBinding
 import com.example.mybaseproject2.databinding.FragmentNotificationsBinding
 
 class Notifications : BaseFragment<FragmentNotificationsBinding>(FragmentNotificationsBinding::inflate) {
